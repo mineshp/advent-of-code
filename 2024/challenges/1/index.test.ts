fdae2cd270b7c68c09b101d30dd9569fef635616
Minesh Patel <mineshpatelis@gmail.com>
2024-12-03 20:54:50 +0000

    Day 1: Part2

--- 2024/challenges/1/index.test.ts
+++ 2024/challenges/1/index.test.ts
@@ -1,11 +1,15 @@
 import { expect, test } from 'bun:test';
-import { handle } from './index';
+import { part1, part2 } from '.';
 
 /*
 
 Maybe the lists are only off by a small amount! To find out, pair up the numbers and measure how far apart they are. Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
 
 */
-test('should sum the diff foreach of the smallest numbers from each list', () => {
-  expect(handle([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3])).toBe(11);
+test('part1: should sum the diff foreach of the smallest numbers from each list', () => {
+  expect(part1([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3])).toBe(11);
+});
+
+test('part2: should return number of times a number in lista appears in listb', () => {
+  expect(part2([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3])).toEqual(31);
 });
